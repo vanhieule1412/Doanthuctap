71161dbbba216d02b85ed610937d77e9bb1bc0d6
KID <vanhieule0299@gmail.com>
2021-04-16 15:30:10 +0700

    thêm chức năng thêm khách hàng

---
+++ doanthuctap/doanthuctap/Controllers/KhachHangController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doanthuctap.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private Models.tinhtiendienEntities dc = new Models.tinhtiendienEntities();
+        // GET: KhachHang
+        public ActionResult IndexKH()//lấy danh sách khách hàng từ csdl
+        {
+            return View(dc.KHANHHANGs.ToList());
+        }
+        public ActionResult Fromthemkhachhang()//hiện thị form thêm khách hàng
+        {
+            
+            return View();
+        }
+        public ActionResult themkhachhang(Models.KHANHHANG kHANHHANG)//hàm xử lý thêm
+        {
+            if (ModelState.IsValid)
+            {
+                dc.KHANHHANGs.Add(kHANHHANG);
+                dc.SaveChanges();
+            }
+            return RedirectToAction("IndexKH");//quay trở lại form IndexKH sau khi thêm xong
+            //vậy là xong thêm
+        }
+
+    }
+}
