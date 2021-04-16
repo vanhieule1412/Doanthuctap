ca2db48c99fc28a743c523f9a3fd6a126b77ac7b
KID <vanhieule0299@gmail.com>
2021-04-16 15:16:36 +0700

    Thêm chức năng thêm giá điện

---
+++ doanthuctap/doanthuctap/Controllers/GiaDienController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doanthuctap.Controllers
+{
+    public class GiaDienController : Controller
+    {
+        private Models.tinhtiendienEntities dc = new Models.tinhtiendienEntities();
+        // GET: GiaDien
+        public ActionResult IndexGD()
+        {
+            return View(dc.GIADIENs.ToList());
+        }
+        public ActionResult Fromthemgiadien()
+        {
+            return View();
+        }
+        public ActionResult themgiadien(Models.GIADIEN gIADIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                dc.GIADIENs.Add(gIADIEN);
+                dc.SaveChanges();
+            }
+            return RedirectToAction("IndexGD");
+        }
+    }
+}
